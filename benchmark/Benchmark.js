8dd18cc6e7d4f15334f2354f35942aace3d16f0d
Denis Fäcke <serayaeryn@gmail.com>
2018-09-02 14:40:40 +0200

    adopt standard

--- benchmark/Benchmark.js
+++ benchmark/Benchmark.js
@@ -1,52 +1,53 @@
-'use strict';
+'use strict'
 
-const benchmark = require('fastbench');
-const FileRotateTransport = require('../');
-const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+const benchmark = require('fastbench')
+const FileRotateTransport = require('../')
+const winston = require('winston')
+const DailyRotateFile = require('winston-daily-rotate-file')
+const path = require('path')
 
-const MESSAGE = Symbol.for('message');
+const MESSAGE = Symbol.for('message')
 
 class JsonFormat {
-  transform(info, opts) {
-    info[MESSAGE] = `{"level":"${info.level}","message":"${info.message}"}`;
-    return info;
+  transform (info, opts) {
+    info[MESSAGE] = `{"level":"${info.level}","message":"${info.message}"}`
+    return info
   }
 }
 
 const winston1 = winston.createLogger({
   transports: [
     new FileRotateTransport({
-      fileName: __dirname + '/console1%DATE%.log',
+      fileName: path.join(__dirname, '/console1%DATE%.log'),
       datePattern: 'DDMMYYYY'
     })
   ],
   format: new JsonFormat()
-});
+})
 
 const winston2 = winston.createLogger({
   transports: [
     new DailyRotateFile({
-      filename: __dirname + '/console2%DATE%.log',
+      filename: path.join(__dirname, '/console2%DATE%.log'),
       datePattern: 'DDMMYYYY'
     })
   ],
   format: new JsonFormat()
-});
+})
 
 const run = benchmark([
   function benchFastFileRotate (cb) {
     for (let i = 0; i < 10; i++) {
-      winston1.log('info', 'hello world!');
+      winston1.log('info', 'hello world!')
     }
-    setImmediate(cb);
+    setImmediate(cb)
   },
   function benchWinstonDailyRotateFile (cb) {
     for (let i = 0; i < 10; i++) {
-      winston2.log('info', 'hello world!');
+      winston2.log('info', 'hello world!')
     }
-    setImmediate(cb);
+    setImmediate(cb)
   }
-], 10000);
+], 10000)
 
-run(run);
+run(run)
--- test/base.test.js
+++ test/base.test.js
@@ -1,187 +1,189 @@
-'use strict';
+'use strict'
 
-const tap = require('tap');
-const test = tap.test;
-const winston = require('winston');
+const tap = require('tap')
+const test = tap.test
+const winston = require('winston')
 const FileRotateTransport = require('..')
-const DateFormat = require('fast-date-format');
-const fs = require('fs-extra');
+const DateFormat = require('fast-date-format')
+const fs = require('fs-extra')
+const path = require('path')
 
-const dateFormat = new DateFormat();
+const dateFormat = new DateFormat()
 
 test('should log into file without callback', (t) => {
-  t.plan(1);
+  t.plan(1)
   cleanUp()
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
   const logger = winston.createLogger({
     transports: [transport]
-  });
-
-  transport.on('logged', () => {
-    t.pass();
   })
 
-  logger.info('debug', 'a message');
-  logger.end();
-  cleanUp();
-});
+  transport.on('logged', () => {
+    t.pass()
+  })
+
+  logger.info('debug', 'a message')
+  logger.end()
+  cleanUp()
+})
 
 test('should call callback on log()', (t) => {
-  t.plan(3);
+  t.plan(3)
   cleanUp()
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
   const logger = winston.createLogger({
     transports: [transport]
-  });
+  })
 
   transport.on('logged', () => {
-    t.pass();
+    t.pass()
   })
 
   transport.log({
-    level: 'debug', 
+    level: 'debug',
     message: 'a message'
   }, (err, logged) => {
-    t.error(err);
-    t.ok(logged);
-  });
-  logger.end();
-  cleanUp();
-});
+    t.error(err)
+    t.ok(logged)
+  })
+  logger.end()
+  cleanUp()
+})
 
 test('should handle missing callback on log()', (t) => {
-  t.plan(1);
+  t.plan(1)
   cleanUp()
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
   const logger = winston.createLogger({
     transports: [transport]
-  });
+  })
 
   transport.on('logged', () => {
-    t.pass();
+    t.pass()
   })
 
   transport.log({
-    level: 'debug', 
+    level: 'debug',
     message: 'a message'
-  });
-  logger.end();
-  cleanUp();
-});
+  })
+  logger.end()
+  cleanUp()
+})
 
 test('should rotate the log file', (t) => {
-  t.plan(1);
+  t.plan(1)
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
-  transport.stream.currentDate = ~~(Date.now() / 86400000) - 1;
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
+  transport.stream.currentDate = ~~(Date.now() / 86400000) - 1
   const logger = winston.createLogger({
     transports: [transport]
-  });
-
-  transport.on('logged', () => {
-    t.pass();
   })
 
-  logger.info('debug', 'a message');
-  logger.end();
-  cleanUp();
-});
+  transport.on('logged', () => {
+    t.pass()
+  })
+
+  logger.info('debug', 'a message')
+  logger.end()
+  cleanUp()
+})
 
 test('should bubble events', (t) => {
-  t.plan(1);
+  t.plan(1)
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
 
   transport.on('error', (err) => {
-    t.ok(err instanceof Error);
-  });
-  const err = new Error('something went wrong');
-  transport.stream.stream.emit('error', err);
+    t.ok(err instanceof Error)
+  })
+  const err = new Error('something went wrong')
+  transport.stream.stream.emit('error', err)
 
-  cleanUp();
-});
+  cleanUp()
+})
 
 test('should wait for close event of stream on close', (t) => {
-  t.plan(2);
+  t.plan(2)
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
 
   transport.on('logged', () => {
-    logger.end();
-  });
+    logger.end()
+  })
   transport.on('close', () => {
-    t.ok(transport.stream.stream.destroyed);
-  });
+    t.ok(transport.stream.stream.destroyed)
+  })
   transport.on('finish', () => {
-    t.pass();
-    cleanUp();
+    t.pass()
+    cleanUp()
   })
   const logger = winston.createLogger({
     transports: [transport]
-  });
+  })
 
-  logger.info('debug', 'a message');
-});
+  logger.info('debug', 'a message')
+})
 
 test('should close ready stream correctly', (t) => {
-  t.plan(2);
+  t.plan(2)
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
 
   transport.stream.stream.on('ready', () => {
-    logger.end();
-  });
+    logger.end()
+  })
   transport.on('close', () => {
-    t.ok(transport.stream.stream.destroyed);
-  });
+    t.ok(transport.stream.stream.destroyed)
+  })
   transport.on('finish', () => {
-    t.pass();
-    cleanUp();
+    t.pass()
+    cleanUp()
   })
   const logger = winston.createLogger({
     transports: [transport]
-  });
+  })
 
-  logger.info('debug', 'a message');
-});
+  logger.info('debug', 'a message')
+})
 
 test('should close new transport correctly', (t) => {
-  t.plan(1);
+  t.plan(1)
 
   const transport = new FileRotateTransport({
-    fileName: __dirname + '/console%DATE%.log'
-  });
+    fileName: path.join(__dirname, '/console%DATE%.log')
+  })
 
   transport.on('finish', () => {
-    t.pass();
-    cleanUp();
+    t.pass()
+    cleanUp()
   })
-  transport.end();
-});
+  transport.end()
+})
 
-function cleanUp() {
-  const fileName = getFileName();
-  if (fs.pathExistsSync(fileName))
-    fs.removeSync(fileName);
+function cleanUp () {
+  const fileName = getFileName()
+  if (fs.pathExistsSync(fileName)) {
+    fs.removeSync(fileName)
+  }
 }
 
-function getFileName() {
-  return __dirname + '/console' + dateFormat.format('DDMMYYYY') + '.log';
+function getFileName () {
+  return path.join(__dirname, '/console', dateFormat.format('DDMMYYYY'), '.log')
 }
